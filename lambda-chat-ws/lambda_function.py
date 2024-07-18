886d0cf250aeb4070c75070f3490d52fa24faacb
Kyoungsu Park <kyopark2014@gmail.com>
2024-07-18 17:39:38 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -959,7 +959,7 @@ def get_rewrite():
     print('langMode: ', langMode)
     
     if langMode:
-        system = """당신은 웹 검색에 최적화된 더 나은 버전의 Question으로 변환하는 질문 re-writer입니다. 질문의 의도와 의미을 잘 표현할 수 있는 한국어 질문을 생성하세요."""
+        system = """당신은 질문 re-writer입니다. 질문의 의도와 의미을 잘 표현할 수 있도록 새로운 질문으로 re-write하세요."""
     else:
         system = """You a question re-writer that converts an input question to a better version that is optimized \n 
         for web search. Look at the input and try to reason about the underlying semantic intent / meaning."""
