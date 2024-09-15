f9f25bd2ad29163b1253c33f83b8c92f740b0a38
Kyoungsu Park <kyopark2014@gmail.com>
2024-09-15 11:51:25 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -988,11 +988,6 @@ def lexical_search_for_tool(query, top_k):
         
     return docs
 
-class GradeDocuments(BaseModel):
-    """Binary score for relevance check on retrieved documents."""
-
-    binary_score: str = Field(description="Documents are relevant to the question, 'yes' or 'no'")
-
 def get_retrieval_grader(chat):
     system = """You are a grader assessing relevance of a retrieved document to a user question. \n 
     If the document contains keyword(s) or semantic meaning related to the question, grade it as relevant. \n
@@ -1350,27 +1345,6 @@ def web_search(question, documents):
             )
         )
     return documents
-
-class GradeDocuments(BaseModel):
-    """Binary score for relevance check on retrieved documents."""
-
-    binary_score: str = Field(description="Documents are relevant to the question, 'yes' or 'no'")
-
-def get_retrieval_grader(chat):
-    system = """You are a grader assessing relevance of a retrieved document to a user question. \n 
-    If the document contains keyword(s) or semantic meaning related to the question, grade it as relevant. \n
-    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
-
-    grade_prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", system),
-            ("human", "Retrieved document: \n\n {document} \n\n User question: {question}"),
-        ]
-    )
-    
-    structured_llm_grader = chat.with_structured_output(GradeDocuments)
-    retrieval_grader = grade_prompt | structured_llm_grader
-    return retrieval_grader
 
 def get_reg_chain():
     if langMode:
